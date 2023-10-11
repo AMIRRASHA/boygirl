50b788908aaef72eeca5b9d31ea0f0069924fc76
AMIRRASHA <129618924+AMIRRASHA@users.noreply.github.com>
2023-10-11 21:06:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Get the input values
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Check if the login data is correct (compare with JSON data)
+    if (isValidLogin(username, password)) {
+        // Hide the login form
+        document.querySelector('.login-box').style.display = 'none';
+
+        // Display the success message
+        document.getElementById('success-message').style.display = 'block';
+    }
+});
+
+function isValidLogin(username, password) {
+    // Replace this with your JSON data or database query
+    const userData = {
+        "aaa": "111",
+        "user2": "password2",
+        // Add more users as needed
+    };
+
+    return userData[username] === password;
+}
